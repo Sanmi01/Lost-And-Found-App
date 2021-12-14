13d27063d85e351f76f33e2102708e486c27bd0e
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2021-12-14 07:52:42 +0100

    add login

---
+++ src/screens/sign-up/SignUp.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import Layout from "../../layouts/Layout";
+
+function SignUp() {
+  return (
+    <Layout>
+      <Container>
+        <div>Sign Up</div>
+      </Container>
+    </Layout>
+  );
+}
+
+export default SignUp;
